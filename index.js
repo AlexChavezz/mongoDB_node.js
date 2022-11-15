359aef975114d3594e23acc65166e2e081fa0f4c
AlexChavezz <alexischavez1234.ac@gmail.com>
2022-11-14 22:22:31 -0600

    insert-docs documentation_added

--- index.js
+++ index.js
@@ -1,21 +1,92 @@
 const { MongoClient } = require("mongodb");
 
-const CONNECTION_STRING  = "mongodb+srv://root:UrHFQ5G25CwP2RLF@cluster0.rrns7um.mongodb.net/?retryWrites=true&w=majority";
+const CONNECTION_STRING = "mongodb+srv://root:UrHFQ5G25CwP2RLF@cluster0.rrns7um.mongodb.net/?retryWrites=true&w=majority";
 const client = new MongoClient(CONNECTION_STRING);
+
+
+// -> Connection to mongoBD 
+
+
+// -> user 
+
+const user = {
+    name: "alexis",
+    age: 21,
+    country: "mexico",
+    isMarried: false,
+}
 
 
 const connectionToMongoDb = async () => {
     try {
         await client.connect();
         console.log("Connected");
-        const dbs =  await client.db().admin().listDatabases();   
+        const dbs = await client.db().admin().listDatabases();
         console.table(dbs.databases);
+        const res = await dbCollection.insertOne(user);
+        console.log(res);
         client.close()
-        .then(console.log("Desconected"))
-    }catch(error)
-    {
+            .then(console.log("Desconected"))
+    } catch (error) {
         console.log(`Error: ${error}`);
     }
 }
+
+// -> CRUD
+
+// -> The first step that you has completed is set your database and your collection
+const dbCollection = client.db("main").collection("mainCollection");
+
+/*
+
+    INSERT DOCUMENTS 
+
+*/
+
+// -> When you want insert only one document you need use the db.collection.insertOne();
+
+async function insertOneDocument(document = {}) {
+    try {
+        const response = await dbCollection.insertOne();
+        console.log(response);
+        // -> IF everything happend ok you should see the next output 
+        /*
+            output: 
+            {
+                acknowledged: true,
+                insertedId: new ObjectId('randomID');
+            }
+        */
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+// -> Also you can insert multiple documents using db.collection.insertMany([documents array])
+
+async function insertManyDocuments(documents = []) {
+    try {
+        const response = await dbCollection.insertMany(documents);
+        console.log(response);
+        // -> Again if all it's ok you see something like
+        /*
+            output: 
+            {
+                acknowledged: true,
+                insertCount: 2,
+                insertedIDs: {
+                    new ObjectId('randomID'),
+                    new ObjectId('randomID')
+                }
+            }
+        */
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+
 
 connectionToMongoDb();
